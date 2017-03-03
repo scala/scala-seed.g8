d44b8c824634fcf171476a0f0bd2f61d9cb5959b
Rui Batista <ruiandrebatista@gmail.com>
2017-03-03 11:38:48 +0000

    compute specs2 version from maven

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val specs2Core = "org.specs2" %% "specs2-core" % specs2Version
 
-  val specs2Version = "3.8.8"
+  val specs2Version = "$specs2_version$"
 }
